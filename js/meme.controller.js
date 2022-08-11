aef2302364f4188d7c7f38f70a17425a27ddbfe4
Zrizony <royyam99@gmail.com>
2022-08-11 16:05:19 +0300

    changed a bit editor layout

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -11,12 +11,19 @@ function onToggleModal() {
 function renderMeme() {
   gElCanvas = document.getElementById('canvas')
   gCtx = canvas.getContext('2d')
+
+  //-- meme object
   const meme = getMeme()
   console.log(' meme:', meme)
 
+  //-- img HTML
   const elImg = document.querySelector(`.img${meme.selectedImgId}`)
   console.log(' elMeme:', elImg)
+
+  //-- canvas settings
   gCtx.drawImage(elImg, 1, 1, elImg.width, elImg.height)
-  gCtx.font = '3rem Ariel'
-  gCtx.fillText('Hello', gElCanvas.width / 2, gElCanvas.height / 2)
+  gCtx.font = '3rem Arial'
+  gCtx.fillText(meme.lines[0].txt, gElCanvas.width / 2, gElCanvas.height / 2)
+  gCtx.strokeStyle = 'red'
+  gCtx.strokeText(meme.lines[0].txt, gElCanvas.width / 2, gElCanvas.height / 2)
 }
